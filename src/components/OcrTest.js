914a6dd08fef198c7747300385d9aae0ff82a75f
Kim gunwoo <odorido1003@gmail.com>
2024-02-21 17:09:29 +0900

    OCR 50%

--- src/components/OcrTest.js
+++ src/components/OcrTest.js
@@ -60,7 +60,7 @@ const OcrTest = () => {
       ],
     });
 
-    const apiKey = '자신의 API KEY 입력';
+    const apiKey = '자신의 API KEY 입력하세요';
     const apiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
 
     try {
